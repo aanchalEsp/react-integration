e08e218c06bd53a04f4d365cccf0de571600fc04
aanchalEsp <aanchal.espsofttech@gmail.com>
2022-06-15 16:10:31 +0530

    initialize react code

---
+++ demo/src/Router/router.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../Component/home';
+import config from '../config/config';
+import Register from '../Component/register'
+import Login from '../Component/login'
+
+import Dashboard from '../Component/dashboard'
+import Profile from '../Component/profile'
+import EditProfile from '../Component/editProfile'
+
+
+
+const RouterComponent = () => {
+    return (
+    
+    <BrowserRouter >
+            <div> 
+                <Routes>
+                    {/* <Route path={`${config.baseUrl}`} element={<Home/>} /> */}
+                    <Route exact path={`${config.baseUrl}register`} element={<Register/>} />
+                    <Route exact path={`${config.baseUrl}dashboard`} element={<Dashboard/>} />
+                    <Route exact path={`${config.baseUrl}profile`} element={<Profile/>} />
+                    <Route exact path={`${config.baseUrl}editprofile`} element={<EditProfile/>} />
+                    <Route exact path={`${config.baseUrl}`} element={<Login/>} /> 
+                </Routes>
+            </div>
+    </BrowserRouter>
+    )
+}
+export default RouterComponent;
